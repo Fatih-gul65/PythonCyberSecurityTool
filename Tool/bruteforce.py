131ccd8b88635b3ebbab90778a319b712d64e50e
mirWalato <walatdundar@gmail.com>
2024-12-16 20:27:50 +0300

    Bruteforce fonksiyonu yazıldı

--- Tool/bruteforce.py
+++ Tool/bruteforce.py
@@ -1,1 +1,26 @@
-print ("hello world")
+﻿sifre = "19649"
+
+rakamlar = "0123456789"
+
+flag = 0
+
+for basamak0 in rakamlar: 
+    for basamak1 in rakamlar:
+        for basamak2 in rakamlar:
+            for basamak3 in rakamlar:
+                for basamak4 in rakamlar:
+                    deneme = (basamak0+basamak1+basamak2+basamak3+basamak4)
+                    print(deneme)
+                    if deneme == sifre:
+                        print ("Parolanız: "+ deneme)
+                        flag = True
+                        break
+                
+                if flag == True:
+                    break
+            if flag == True:
+                    break
+        if flag == True:
+                 break   
+    if flag == True:
+                 break    
