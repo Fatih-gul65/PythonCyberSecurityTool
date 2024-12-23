600e6fe61398bc52d85a05a8ee6f0f57bcaae074
CYLOX <CYLOX@DESKTOP-3L4M0RL>
2024-12-23 20:36:59 +0300

    PortScanner ek düzneleme

--- Tool/PortScanner.py
+++ Tool/PortScanner.py
@@ -1,8 +1,9 @@
+# -*- coding: utf-8 -*-
 import threading
 import socket
 
 # Port tarama fonksiyonu
-def portara(ip, port):
+def portarama(ip, port):
     try:
         soket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         soket.settimeout(0.5)
@@ -23,7 +24,7 @@ def threadile(hedef1, hedef2, portlar):
         hedef_ip = hedef1.split('.')[0] + '.' + hedef1.split('.')[1] + '.' + hedef1.split('.')[2] + '.' + str(sonblok)
         portlar_listesi = portlar.split(',')
         for port in portlar_listesi:
-            thread = threading.Thread(target=portara, args=(hedef_ip, int(port)))
+            thread = threading.Thread(target=portarama, args=(hedef_ip, int(port)))
             threadliste.append(thread)
             thread.start()
 
@@ -32,7 +33,7 @@ def port_aralik_tarama(hedef, baslangic_portu, bitis_portu):
     print('Tarama Baþlýyor.....')
     print('*'*50)
     for port in range(baslangic_portu, bitis_portu+1):
-        portara(hedef, port)
+        portarama(hedef, port)
 
 # Kullanýcýdan seçim al
 print("1 - IP Aralýðýnda Belirli Portlarý Tara")
@@ -40,10 +41,10 @@ print("2 - Tek Bir IP Ýçin Port Aralýðýný Tara")
 secim = input("Yapmak istediðiniz iþlemi seçin (1 veya 2): ")
 
 if secim == '1':
-    _ip1 = input('Hedefin ilk IP adresini giriniz: ')
-    _ip2 = input('Hedefin son IP adresini giriniz: ')
+    ip_araligi = input('Hedef IP aralýðýný giriniz (örnek: 192.168.1.1-192.168.1.5): ')
     _portlar = input('Virgül ile ayýrarak portlarý giriniz: ')
-    threadile(_ip1, _ip2, _portlar)
+    _ip1, _ip2 = ip_araligi.split('-')  # "-" ile ayýrarak iki IP'yi alýyoruz
+    threadile(_ip1.strip(), _ip2.strip(), _portlar)  # Strip ile boþluklarý temizliyoruz
     input('Taramayý tamamlamak için bekleyin...')
 elif secim == '2':
     _ip = input('Hedefin IP adresini giriniz: ')
