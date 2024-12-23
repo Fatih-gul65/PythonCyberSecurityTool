50391539f8983be796e3b2e1f18563508f8141a3
CYLOX <CYLOX@DESKTOP-3L4M0RL>
2024-12-23 20:47:52 +0300

    WifiScanner düzenleme

--- Tool/WifiScanner.py
+++ Tool/WifiScanner.py
@@ -2,15 +2,15 @@ import subprocess
 import re
 
 def scan_wifi():
-    # Wi-Fi aðlarýný taramak için netsh komutunu çalýþtýr
+    # Wi-Fi networks scanning using netsh command
     try:
         command = "netsh wlan show networks mode=bssid"
-        # encoding'i 'latin-1' olarak deðiþtiriyoruz
+        # Changing encoding to 'latin-1'
         output = subprocess.check_output(command, shell=True, text=True, encoding='latin-1')
         
         networks = []
         
-        # Çýktýyý satýr satýr analiz et
+        # Analyzing the output line by line
         for line in output.split('\n'):
             if "SSID" in line and "BSSID" not in line:
                 ssid = re.search(r"SSID \d+ : (.+)", line)
@@ -27,14 +27,14 @@ def scan_wifi():
         
         return networks
     except Exception as e:
-        print(f"Hata: {e}")
+        print(f"Error: {e}")
         return []
 
-# Aðlarý tarayýp sonuçlarý göster
+# Scanning networks and displaying results
 wifi_networks = scan_wifi()
 if wifi_networks:
-    print("Bulunan Wi-Fi Aðlarý:")
+    print("Detected Wi-Fi Networks:")
     for network in wifi_networks:
-        print(f"SSID: {network.get('SSID')}, Þifreleme: {network.get('Authentication')}, Sinyal Gücü: {network.get('Signal Strength')}%")
+        print(f"SSID: {network.get('SSID')}, Encryption: {network.get('Authentication')}, Signal Strength: {network.get('Signal Strength')}%")
 else:
-    print("Hiçbir að bulunamadý.")
+    print("No networks found.")
